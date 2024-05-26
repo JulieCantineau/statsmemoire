3799f753e430ebfe9d56f70d8e76799c50bd570c
JulieCantineau <julie.cantineau@student.umons.ac.be>
2024-05-26 14:56:04 +0200

    stress ox ctl +

--- Test-normalité.R
+++ Test-normalité.R
@@ -23,7 +23,31 @@ car::qqPlot(AnovaModel.Viab48h$residuals)
 
 --------------------------------------------------
   
-  
-  
-kruskal.test(Valeurs ~ Conditions, data = Viab48h)
+test <- kruskal.test(Valeurs ~ Conditions, data = Viab48h)
 dunnTest(Valeurs ~ Conditions, data=Viab48h, method="hs")
+
+summary(comp_a <- nparcomp(Valeurs ~ Conditions, data=Viab48h, type="Dunnett", control = "CTL"))
+plot(comp_a)
+
+
+  
+shapiro.test(AnovaModel.sox1h$residuals)
+car::qqPlot(AnovaModel.sox1h$residuals)
+
+shapiro.test(AnovaModel.sox30m$residuals)
+car::qqPlot(AnovaModel.sox30m$residuals)
+
+shapiro.test(AnovaModel.sox2h$residuals)
+car::qqPlot(AnovaModel.sox2h$residuals)
+
+shapiro.test(AnovaModel.sox4h$residuals)
+car::qqPlot(AnovaModel.sox4h$residuals)
+
+shapiro.test(AnovaModel.sox6h$residuals)
+car::qqPlot(AnovaModel.sox6h$residuals)
+
+shapiro.test(AnovaModel.sox24h$residuals)
+car::qqPlot(AnovaModel.sox24h$residuals)
+
+shapiro.test(AnovaModel.sox48h$residuals)
+car::qqPlot(AnovaModel.sox48h$residuals)

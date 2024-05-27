b1fe64aae1a2b9db527a2be4cf6db69c4dd752e2
JulieCantineau <julie.cantineau@student.umons.ac.be>
2024-05-27 20:04:41 +0200

    Sox normalisé sur ctl neg

--- Test-normalité.R
+++ Test-normalité.R
@@ -21,16 +21,19 @@ shapiro.test(AnovaModel.Viab48h$residuals)
 car::qqPlot(AnovaModel.Viab48h$residuals)
 
 
---------------------------------------------------
   
+  
+library(nparcomp)
 test <- kruskal.test(Valeurs ~ Conditions, data = Viab48h)
 dunnTest(Valeurs ~ Conditions, data=Viab48h, method="hs")
 
 summary(comp_a <- nparcomp(Valeurs ~ Conditions, data=Viab48h, type="Dunnett", control = "CTL"))
 plot(comp_a)
 
+summary(comp_b <- nparcomp(Valeurs ~ Conditions, data=Viab48h, type="Tukey", control = "CTL"))
+plot(comp_b)
 
-  
+
 shapiro.test(AnovaModel.sox1h$residuals)
 car::qqPlot(AnovaModel.sox1h$residuals)
 
@@ -51,3 +54,21 @@ car::qqPlot(AnovaModel.sox24h$residuals)
 
 shapiro.test(AnovaModel.sox48h$residuals)
 car::qqPlot(AnovaModel.sox48h$residuals)
+
+shapiro.test(AnovaModel.sox30mCN$residuals)
+car::qqPlot(AnovaModel.sox30mCN$residuals)
+
+shapiro.test(AnovaModel.sox1hCN$residuals)
+car::qqPlot(AnovaModel.sox1hCN$residuals)
+
+shapiro.test(AnovaModel.sox2hCN$residuals)
+car::qqPlot(AnovaModel.sox2hCN$residuals)
+
+shapiro.test(AnovaModel.sox4hCN$residuals)
+car::qqPlot(AnovaModel.sox4hCN$residuals)
+
+shapiro.test(AnovaModel.sox6hCN$residuals)
+car::qqPlot(AnovaModel.sox6hCN$residuals)
+
+shapiro.test(AnovaModel.sox24hCN$residuals)
+car::qqPlot(AnovaModel.sox24hCN$residuals)

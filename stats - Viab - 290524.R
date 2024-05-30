647d6ddf5831eeefc81686f19b0ee9d4da1f4a3d
JulieCantineau <julie.cantineau@student.umons.ac.be>
2024-05-30 10:12:44 +0200

    préparation pour viab

---
+++ stats - Viab - 290524.R
@@ -0,0 +1,18 @@
+library(readxl)
+library(tidyverse)
+library(rstatix)
+library(ggpubr)
+
+Viab1h <- read_excel("E:/Mémoire/Viab-SOx/1h/Ros1H.xlsx")
+
+Viab2h <- read_excel("E:/Mémoire/Viab-SOx/2h/Ros2H.xlsx")
+
+Viab4h <- read_excel("E:/Mémoire/Viab-SOx/4h/Ros4H.xlsx")
+
+Viab6h <- read_excel("E:/Mémoire/Viab-SOx/6h/Ros6H.xlsx")
+
+Viab24h <- read_excel("E:/Mémoire/Viab-SOx/24h/Ros24H.xlsx")
+
+Viab48h <- read_excel("E:/Mémoire/Viab-SOx/48h/Ros48H.xlsx")
+
+Viab30m <- read_excel("E:/Mémoire/Viab-SOx/30m/Ros30m.xlsx")
